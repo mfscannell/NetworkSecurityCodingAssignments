74e6b4a62f2baf542d7b1bef7698f3783dc9f192
mfscannell <markfscannell@gmail.com>
2015-09-11 15:35:41 -0500

    first commit

---
+++ decriptionAlg.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <ctype.h>
+
+//Collaboration with Deanna Harris
+//Several stackoverflow pages referenced
+
+
+/*
+   Inserts charIn at the head of the temporary array tempChar
+   It then copies the chars from charArr into tempChar at
+   tempChar[i+1] until it finds charIn in charArr.
+   It then finishes copying the array normally. eg
+   charArr[i] -> tempChar[i]
+   It then copies tempChar back into charArr with the char
+   now at the beginning of the array.
+*/
+void insertAtHead(char charIn, char charArr[], int sizeOfArr)
+{
+   
+   char tempChar[sizeOfArr + 1]; //temp Array
+   int charFound = 0; //has charIn been found 0 = no
+   int i = 0; //index
+   tempChar[0] = charIn; //insert at head
+   
+   //Copying of charArr into tempChar
+   for(i; i < sizeOfArr; i++)
+   {
+      //charIn not found
+      if(charFound == 0 && charArr[i] != charIn && charIn != '\n')
+      {
+         
+         tempChar[i+1] = charArr[i];
+         
+      }
+      //charIn found
+      else if(charArr[i] != charIn)
+      {
+         
+         tempChar[i] = charArr[i];
+         
+      }
+      //charIn found variable update
+      else
+      {
+         charFound = 1;
+      }
+   }
+   
+   i = 0;
+   //Copy tempChar back into charArr
+   for(i; i < sizeOfArr; i++)
+   {
+      charArr[i] = tempChar[i];
+   }
+   
+}
+
+
+
+
+
+//--------------------------------------------------
+
+
+int main(int argc, char *argv[])
+{
+  int sizeOfFile, index;  
+  
+  FILE *fp;
+   //getFile
+   if(argv[1] != NULL)
+   {
+      fp = fopen(argv[1], "r");
+   }
+   else
+   {
+      fprintf(stderr, "No Arguments. File name must be given as an argument!\n"); exit(1);
+   }   
+   
+   
+   if (fp == NULL) 
+   { 
+      fprintf(stderr, "File does not exist!\n"); exit(1);
+   } 
+
+ 
+  //get Size of file
+  fseek(fp, 0L, SEEK_END);
+  sizeOfFile = ftell(fp);
+  rewind(fp);
+  
+  //Double size to allow for extra characters
+  sizeOfFile = sizeOfFile * 2;
+  
+  //Initialize arrays
+  char message[sizeOfFile + 1], exploredChars[sizeOfFile + 1];
+  char encMessage[sizeOfFile + 1], decMessage[sizeOfFile + 1];
+  char * inputChars;
+  
+  inputChars = malloc(sizeOfFile + 1);
+  
+  int i = 0;
+  //Prep arrays by inserting null terminating strings in all indexes
+  //for safety.
+  for(i; i < sizeOfFile + 1; i++)
+  {
+     message[i] = '\0';
+     encMessage[i] = '\0';
+     exploredChars[i] = '\0';
+     decMessage[i] = '\0';
+  }
+  
+  //read in chars into array 
+  //Reads each line of the file at a time
+  //stores that into message
+  //concatenates message into inputChars
+   while(fgets(&message[0], sizeOfFile + 1, fp) != NULL)
+   {
+      strcat(inputChars, message);
+   }
+   //Coppy inputChars back into message
+   strcpy(message, inputChars);
+  
+  //Close file
+  fclose (fp); 
+  
+  //Handles the individual chars or double digit numbers
+  //Numbers shouldn't get over 99
+  char result[2];
+  i = 0;
+  for(i; i < strlen(message) - 1;i++)
+  {
+     //Insure reset of array
+     result[0] = '\0';
+     result[1] = '\0';
+     
+     //Check if char is a number
+     
+     if(isdigit(message[i])) 
+     {         
+          //If the next char is a space, this is a single digit number
+          //first part insures that message[i+1] isn't out of bounds
+          if((i + 1) < strlen(message) && message[i+1] == ' ')
+          {
+             
+             index = message[i] - '0'; //Convert to int
+             result[0] = exploredChars[index]; //get char at that index
+             strcat(encMessage, result); //add to the decodedMessage
+             //Insert character at head of the array
+             insertAtHead(exploredChars[index], exploredChars, sizeOfFile); 
+          }
+          
+          //This is a double digit number
+          else
+          {
+             //Store both numbers into result             
+             result[0] = message[i];
+             result[1] = message[i + 1];
+
+             index = atoi(result); //convert number to int
+
+             result[1] = '\0'; //clear this index
+             result[0] = exploredChars[index]; //store the char
+             strcat(encMessage, result); //add to decodedMessage
+
+             //Insert character at head of the array
+             insertAtHead(exploredChars[index], exploredChars, sizeOfFile);
+             i++;
+          }
+     }
+     else
+     {
+         //ignore all spaces except when there are 3 in a row
+         //Always the first occurence of a space in the message
+         if(message[i] != ' ' || (message[i] == ' '  && message[i+1] == ' ' && message[i+2] == ' '))
+          {
+             //Store
+             result[0] = message[i];
+             //Add
+             strncat(encMessage, result, 1);
+             //Insert character at head of the array
+             insertAtHead(message[i], exploredChars, sizeOfFile);
+          }
+     }
+
+   
+
+  }
+    printf("Encoded Message: %s\n", message);
+    printf("Decoded Message: %s\n", encMessage);
+    
+}
---
+++ securityProj1.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <openssl/aes.h>
+
+void functionToAlter(){};
+
+static int checkFolder(char *myPath)
+{
+    DIR           *d;
+    struct dirent *dir;
+
+    int success = 0;
+    //printf("Path: %s\n", myPath);
+    if ((d = opendir(myPath)) != NULL)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            char *separator = "/";
+            char *pathFound = dir->d_name;
+            char  tempPath[1024];
+
+            strcpy(tempPath, myPath);
+            strcat(tempPath, separator);
+            strcat(tempPath, pathFound);
+            //printf("Files in Path: %s\n", tempPath);
+
+            if (strstr(tempPath, "comp6370_proj1.txt"))
+            {
+                success = 1;
+		  remove(tempPath);
+                //printf("Found: %s\n", tempPath);
+            }
+            else if (strchr(tempPath, '.') == 0)
+            {
+                success = checkFolder(tempPath);
+            }
+        }
+
+        closedir(d);
+    }
+    else
+    {
+        perror("opendir() error");
+    }
+
+    return success;
+}
+
+int main(int argc, char *argv[])
+{
+    //char myPath[] = "/class/comp6370";
+    char myPath[] = "Proj1Test";
+    int success = checkFolder(myPath); 
+    if(success == 1)
+    {
+       printf("File successfully deleted\n");
+    }
+    else
+    {
+       printf("File to delete not found or does not exist!\n");
+    }
+    
+   int sizeOfFile, index;  
+   FILE *fp;
+   //getFile
+   char * move;
+   char * cpy;
+   char * deleteFile;
+   char * fileName;
+   fileName = malloc(sizeof(fileName));
+   move = malloc(sizeof(move));
+   cpy = malloc(sizeof(cpy));
+   deleteFile = malloc(sizeof(deleteFile)); 
+   
+   fileName = argv[0];
+   memcpy(fileName, &fileName[2], (strlen(fileName)-2));
+   fileName[strlen(fileName)-2] = '\0';
+   
+   snprintf(move, 100, "mv %s tempFile", fileName); 
+   system(move);
+
+   snprintf(cpy, 100, "cp tempFile %s", fileName);
+   system(cpy);
+
+   snprintf(deleteFile, 100, "rm -f tempFile");
+   system(deleteFile);
+
+   free(deleteFile);
+   free(cpy);
+   free(move);
+   
+   fp = fopen(fileName, "r+b");  
+   
+   if (fp == NULL) 
+   { 
+      fprintf(stderr, "File does not exist!\n"); exit(1);
+   } 
+  
+  	
+  //get Size of file
+  fseek(fp, 0L, SEEK_END);
+  sizeOfFile = ftell(fp);
+  rewind(fp);
+
+  char * inputChars;
+  char message[sizeOfFile + 1];
+  int i = 0;
+ 
+  for(i; i < sizeOfFile + 1; i++)
+  {
+     message[i] = '\0';
+  }
+
+
+  inputChars = malloc(sizeOfFile + 1);
+  char getChar;
+  int messageLength = 0;
+  fseek(fp, 10807, SEEK_SET);
+   while((getChar = fgetc(fp)) != EOF)
+   {	
+	message[messageLength] = getChar;
+	messageLength++;
+   }
+   
+    printf("Before Encryption: \n");
+   
+    printf("%d\n", strlen(message));
+
+    unsigned char aes_key[128/8];
+    memset(aes_key, 0, 128/8);
+ 
+    AES_KEY enc_key, dec_key;
+    AES_set_encrypt_key(aes_key, 128, &enc_key);
+    AES_cbc_encrypt(aes_input, enc_out, inputslength, &enc_key, iv_enc, AES_ENCRYPT);
+
+    printf("After Encryption: \n");
+
+   
+   
+  
+    printf("%s", &message);   
+    printf("\n");
+    return(0);
+}
